fe91633c0ff96548e73b682608e38a2f43e592c3
ko jaewoo <jaewoo.ko@truabutment.com>
2023-11-24 17:20:15 +0900

    'selenium' to 'puppeteer'

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,20 +2,28 @@ import { SisoService } from './siso/siso.service';
 import { Controller, Get, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
+import puppeteer from 'puppeteer';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly sisoService: SisoService,
-  ) {}
+  ) { }
 
   @Get()
   @Render('index')
   async index() {
     let result = {};
     try {
-      result = await this.sisoService.doReservation();
+      const browser = await puppeteer.launch({
+        headless: false,
+        executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+      });
+      const page = await browser.newPage();
+      await page.goto('https://share.siheung.go.kr');
+      await new Promise(resolve => setTimeout(resolve, 5000));
+      await browser.close();
     } catch (error) {
       console.log(error);
       return { data: null, error: error };
