66cd9924c32862eeed0b332bdae092c899c146d4
ko jaewoo <jaewoo.ko@truabutment.com>
2024-01-16 10:04:16 +0900

    set headless

--- src/main/services/SisoService.ts
+++ src/main/services/SisoService.ts
@@ -33,7 +33,8 @@ class SisoService {
         this.browser = await puppeteer.launch({
             executablePath:
                 'C:/Program Files/Google/Chrome/Application/chrome.exe',
-            headless: global.isDev,
+            // headless: !global.isDev,
+            headless: true,
             defaultViewport: { width: 1920, height: 1080 }, // 브라우저 창 크기 설정 (기본값: 800x600)
             args: ['--start-maximized'], // 최대화된 창으로 시작
         });
