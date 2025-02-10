ebb61844188780899588f35f45ac8cfd65c743fb
helperwoo <helperwoo.ko@gmail.com>
2025-02-10 17:00:08 +0900

    main.ts 수정

--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -2,22 +2,24 @@ import { app } from 'electron';
 import windowService from './services/WindowService';
 
 const gotTheLock = app.requestSingleInstanceLock();
-if (!gotTheLock) app.quit();
+if (!gotTheLock) {
+    app.quit();
+} else {
+    app.on('second-instance', () => {
+        if (windowService.getWindow().isMinimized() || !windowService.getWindow().isVisible()) {
+            windowService.getWindow().show();
+        }
+        windowService.getWindow().focus();
+    });
 
-app.whenReady().then(async () => {
-    windowService.createWindow();
-    windowService.loadWindow();
-})
+    app.whenReady().then(async () => {
+        windowService.createWindow();
+        windowService.loadWindow();
+    })
 
-app.on('second-instance', () => {
-    if (windowService.getWindow().isMinimized() || !windowService.getWindow().isVisible()) {
-        windowService.getWindow().show();
-    }
-    windowService.getWindow().focus();
-});
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-})
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
+    })
+}
--- packages/common/src/const/BookStatus.ts
+++ packages/common/src/const/BookStatus.ts
@@ -1,1 +1,1 @@
-export const enum BookStatus { stop, wait, run }
+export enum BookStatus { stop, wait, run }
